d31c4c01e3a826dd1dbdc7ad87a67bc6600f2bc5
rom3lll <romelsilvera@gmail.com>
2021-08-14 17:18:20 -0400

    Alarm Test and Assertions done.

--- src/test/java/activity/AlarmScreen.java
+++ src/test/java/activity/AlarmScreen.java
@@ -1,12 +1,14 @@
 package activity;
 
 import controlAppium.Button;
+import controlAppium.Label;
 import controlAppium.TextBox;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import session.Session;
 
 import java.net.MalformedURLException;
+import java.util.List;
 
 public class AlarmScreen {
 
@@ -18,6 +20,10 @@ public class AlarmScreen {
     public Button repeatButton = new Button(By.id("com.google.android.deskclock:id/repeat_onoff"));
     public Button setTypeButton = new Button(By.id("android:id/toggle_mode"));
     public TextBox descriptionAlarmText = new TextBox(By.xpath("//android.widget.FrameLayout/alf/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.EditText"));
+    public Label noAlarmText = new Label(By.xpath("//android.widget.TextView[@content-desc='No Alarms']"));
+    public Label dayDescriptionText=new Label(By.id("com.google.android.deskclock:id/upcoming_instance_label"));
+
+    private String alarmListButton = "(//android.support.v7.widget.RecyclerView/android.view.ViewGroup)";
 
     public AlarmScreen() {
     }
@@ -57,5 +63,47 @@ public class AlarmScreen {
         }
         WebElement element = Session.getInstance().getDriver().findElement(By.xpath(meridianXpath));
         element.click();
+    }
+
+    public void deleteAlarmButton() throws MalformedURLException {
+
+        while (!isEmptyList()) {
+                if (countAlarmButton()==0){
+                    break;
+                }
+                WebElement element = Session.getInstance().getDriver().findElement(By.xpath(alarmListButton + "[1]"));
+                element.click();
+                deleteAlarmButton.click();
+
+
+        }
+    }
+
+    public int countAlarmButton() throws MalformedURLException {
+        List<WebElement> elements = Session.getInstance().getDriver().findElements(By.xpath(alarmListButton));
+        return elements.size();
+    }
+
+    private boolean isEmptyList() throws MalformedURLException {
+        return (countAlarmButton() == 0) ? true : false;
+    }
+
+    public String alarmExistsByDescription(String descriptionText) throws MalformedURLException {
+        try {
+            WebElement element = Session.getInstance().getDriver().findElement(By.xpath("//android.widget.TextView[@content-desc='Label " + descriptionText + "']"));
+            System.out.println("//android.widget.TextView[@content-desc='" + descriptionText + "']");
+            return element.getText();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public String getAlarmTime() throws MalformedURLException {
+        try {
+            WebElement element = Session.getInstance().getDriver().findElement(By.id("com.google.android.deskclock:id/digital_clock"));
+            return element.getText().replaceAll("[^\\d\\w:]","");
+        } catch (Exception e) {
+            return "";
+        }
     }
 }
--- src/test/java/test/AlarmTest.java
+++ src/test/java/test/AlarmTest.java
@@ -3,67 +3,94 @@ package test;
 
 import activity.AlarmScreen;
 import activity.HeaderScreen;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import session.Session;
+
 import java.net.MalformedURLException;
+
 
 public class AlarmTest {
 
 
-
     @BeforeEach
     public void setUp() throws MalformedURLException {
-        HeaderScreen headerScreen=new HeaderScreen();
+        HeaderScreen headerScreen = new HeaderScreen();
         headerScreen.goToAlarmScreen();
+        AlarmScreen alarmScreen = new AlarmScreen();
+        alarmScreen.deleteAlarmButton();
     }
 
     @Test
     public void Should_CreateAnAlarm() throws MalformedURLException {
-        AlarmScreen alarmScreen= new AlarmScreen();
+        AlarmScreen alarmScreen = new AlarmScreen();
         alarmScreen.addAlarmButton.click();
         alarmScreen.saveAlarmButton.click();
         alarmScreen.closeAlarmSettingsButton.click();
+
+        boolean actualResult = (alarmScreen.countAlarmButton() >= 1) ? true : false;
+        boolean expectedResult = true;
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
+
     }
 
 
     @Test
     public void Should_CreateAnAlarmAndDelete() throws MalformedURLException {
-        AlarmScreen alarmScreen= new AlarmScreen();
+        AlarmScreen alarmScreen = new AlarmScreen();
         alarmScreen.addAlarmButton.click();
         alarmScreen.saveAlarmButton.click();
         alarmScreen.deleteAlarmButton.click();
+
+        String actualResult = alarmScreen.noAlarmText.getText();
+        String expectedResult = "No Alarms";
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
     }
+
     @Test
     public void Should_CreateAndWriteDescription() throws MalformedURLException {
-        AlarmScreen alarmScreen= new AlarmScreen();
+        AlarmScreen alarmScreen = new AlarmScreen();
         alarmScreen.addAlarmButton.click();
         alarmScreen.saveAlarmButton.click();
         alarmScreen.typeDescriptionAlarm("Test Homework");
 
+        String actualResult = alarmScreen.alarmExistsByDescription("Test Homework");
+        String expectedResult = "Test Homework";
+        Assertions.assertEquals(actualResult, expectedResult, "Error");
     }
 
     @Test
-    public void Should_SelectAlarmDay() throws MalformedURLException {
-        AlarmScreen alarmScreen= new AlarmScreen();
+    public void Should_CreateAlarmForTomorrow() throws MalformedURLException {
+        AlarmScreen alarmScreen = new AlarmScreen();
         alarmScreen.addAlarmButton.click();
         alarmScreen.saveAlarmButton.click();
-        alarmScreen.repeatButton.click();
-        alarmScreen.selectDay("monday");
         alarmScreen.closeAlarmSettingsButton.click();
+
+        String actualResult = alarmScreen.dayDescriptionText.getText();
+        String expectedResult = "Tomorrow";
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
     }
+
     @Test
     public void Should_TypeAlarmTime() throws MalformedURLException {
-        AlarmScreen alarmScreen= new AlarmScreen();
+        int hour = 7;
+        int minute = 30;
+        String meridian = "am";
+
+        AlarmScreen alarmScreen = new AlarmScreen();
         alarmScreen.addAlarmButton.click();
         alarmScreen.setTypeButton.click();
-        alarmScreen.typeTime(7,30,"am");
+        alarmScreen.typeTime(hour, minute, meridian);
         alarmScreen.saveAlarmButton.click();
         alarmScreen.closeAlarmSettingsButton.click();
+
+        String actualResult = alarmScreen.getAlarmTime();
+        String expectedResult = "7:30AM";
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
     }
+
     @AfterEach
     public void tearDown() throws MalformedURLException {
         Session.getInstance().closeSession();
     }
+
 }
